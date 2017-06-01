549557d632c1fe3ac6aedec739f5d63c152a5246
alexbaker3 <alexbaker3@users.noreply.github.com>
2017-06-01 13:26:27 -0500

    Session 1

---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+
+validates :photo_id, :presence => true
+validates :user_id, :presence => true, :uniqueness => { :scope => :photo }
+belongs_to :user
+belongs_to :photo
+end
